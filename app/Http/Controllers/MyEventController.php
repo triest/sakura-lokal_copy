fd170ffe6864455491a9d2454978ba42c197d3b9
triest <you@example.com>
2019-09-08 17:31:50 +0300

    Fix remidate2

--- app/Http/Controllers/MyEventController.php
+++ app/Http/Controllers/MyEventController.php
@@ -629,7 +629,7 @@ WHERE `myeven`.`organizer_id`=? and `eventreq`.`status`=\'unread\'',
               left join new_chat.myevents even on req.event_id=even.id 
               left join girls on req.girl_id=girls.id 
               left join event_photos photos on photos.myevent_id=even.id
-              where req.girl_id=1 and DATE(even.begin)=CURDATE()
+              where req.girl_id=? and DATE(even.begin)=CURDATE()
               and  	alert_notification_today_received=0
               and (TIMESTAMPDIFF(HOUR, alert_notification_today, NOW())>2)
               ',
